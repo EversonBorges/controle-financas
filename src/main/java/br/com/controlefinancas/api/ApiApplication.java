16694c07412dfd36b46e43cce3a61816acfa8e00
EversonBorges <everson.cursos@gmail.com>
2023-03-14 21:05:58 -0300

    Alterando serviços para atender ao front

--- src/main/java/br/com/controlefinancas/api/ApiApplication.java
+++ src/main/java/br/com/controlefinancas/api/ApiApplication.java
@@ -2,6 +2,9 @@ package br.com.controlefinancas.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -10,4 +13,14 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/cards").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/transactions").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
